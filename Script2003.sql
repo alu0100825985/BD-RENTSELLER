f39a688aedfe43f24dac35632f9a089cf453e552
alu0100825985 <alu0100825985@ull.edu.es>
2017-01-29 21:49:02 +0000

    Terminado el codigo en SQL 2003

--- Script2003.sql
+++ Script2003.sql
@@ -59,3 +59,88 @@ CREATE TYPE Trabajador AS (
 CREATE TABLE Tabla_Trabajador OF Trabajador
 (CIF PRIMARY KEY,
 CodigoSeguridadSocial PRIMARY KEY);
+
+/* ---------------- Tabla Alquilar ---------------- */
+
+CREATE TYPE Alquilar AS (
+	CodigoAlquiler		    VARCHAR(20),
+	NombreConductor			VARCHAR(50),
+	DNIConductor			VARCHAR(9),
+	LugarRecepcion			VARCHAR(30),
+	LugarDevolucion			VARCHAR(30),
+	PrecioAlquiler			NUMBER,
+	FechaFin				DATE,
+	FechaInicio				DATE,
+	Hora					TIME);
+
+CREATE TABLE Tabla_Alquilar OF Alquilar
+(CodigoAlquiler PRIMARY KEY);
+
+/* ---------------- Tabla Comprar ---------------- */
+
+CREATE TYPE Comprar AS (
+	CodigoCompra		    VARCHAR(20),
+	Fecha 					DATE,
+	PrecioCompra			NUMBER);
+
+CREATE TABLE Tabla_Comprar OF Comprar
+(CodigoCompra PRIMARY KEY);
+
+/* ---------------- Tabla Vehiculo ---------------- */
+
+CREATE TYPE Vehiculo AS (
+	CodigoChasis(VIN)		VARCHAR(20),
+	Estado 					VARCHAR(10),
+	Color					VARCHAR(15),
+	Plazo					VARCHAR(15),
+	NumeroUsos				NUMBER);
+
+CREATE TABLE Tabla_Vehiculo OF Vehiculo
+(CodigoChasis(VIN) PRIMARY KEY);
+
+/* ---------------- Tabla Catalogo ---------------- */
+
+CREATE TYPE Catalogo AS (
+	Modelo					VARCHAR(30),
+	Marca 					VARCHAR(30),
+	NumeroPuertas			NUMBER,
+	NumeroAsientos			NUMBER,
+	CantidadTotal			NUMBER,
+	CantidadDisponible		NUMBER,
+	Fianza					REF Deportivo,
+	Tipo					REF Caravana,
+	PesoMaxCarga			REF Utilitario);
+
+CREATE TYPE Deportivo AS (
+	Fianza					NUMBER,
+	Modelo					REF Catalogo,
+	Marca 					REF Catalogo,
+	NumeroPuertas			REF Catalogo,
+	NumeroAsientos			REF Catalogo,
+	CantidadTotal			REF Catalogo,
+	CantidadDisponible		REF Catalogo);
+
+CREATE TYPE Caravana AS (
+	Tipo					VARCHAR(15),
+	Modelo					REF Catalogo,
+	Marca 					REF Catalogo,
+	NumeroPuertas			REF Catalogo,
+	NumeroAsientos			REF Catalogo,
+	CantidadTotal			REF Catalogo,
+	CantidadDisponible		REF Catalogo);
+
+CREATE TYPE Utilitario AS (
+	PesoMaxCarga			NUMBER,
+	Modelo					REF Catalogo,
+	Marca 					REF Catalogo,
+	NumeroPuertas			REF Catalogo,
+	NumeroAsientos			REF Catalogo,
+	CantidadTotal			REF Catalogo,
+	CantidadDisponible		REF Catalogo);
+
+CREATE TABLE Tabla_Catalogo OF Catalogo
+(Modelo PRIMARY KEY,
+Fianza	NULL,
+Tipo 	NULL,
+PesoMaxCarga NULL);
+
